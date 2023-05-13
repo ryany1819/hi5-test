13fbb250eb182b00a5fa6594df337d5ac71dd194
Ryan Yang <ryany1819@gmail.com>
2023-05-13 09:01:37 +0800

    more code completion -- add exclamation icon right to input field, as mentioned in req. 2

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,6 +4,7 @@ import sideImage from './assets/images/side.jpg';
 import TokenContext from './TokenContext';
 import { userLoginApi } from './services';
 import { Navigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function SignIn() {
     const [username, setUsername] = useState('');
@@ -11,7 +12,7 @@ export default function SignIn() {
     const [unameErr, setUnameErr] = useState(0);
     const [pwdErr, setPwdErr] = useState(0);
 
-    const {token, setToken} = useContext(TokenContext);
+    const { token, setToken } = useContext(TokenContext);
 
     const handleClick = (e) => {
         const newUnameErr = validateUsername(username);
@@ -36,35 +37,45 @@ export default function SignIn() {
 
     return (
         <>
-        {token && (<Navigate to="/" />)}
-        <div id="signin">
-            <div id="signin-left">
-                <div id="signin-card">
-                    <div id="signin-logo"><b>LOGO</b></div>
-                    <h1>Sign In</h1>
-                    <p id="signin-subtitle">Sign in to stay connected.</p>
-                    <form onSubmit={doLogin}>
-                        <label className="input-label" htmlFor="username">Username</label>
-                        <input type="text" className={`${unameErr ? "red-border" : "blue-border"}`} name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                        {unameErr !== 0 && <p className="err-msg">{UNAME_ERR_MSG[unameErr]}</p>}
-                        <label className="input-label" htmlFor="password">Password</label>
-                        <input type="password" className={`${pwdErr ? "red-border" : "blue-border"}`} name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        {pwdErr !== 0 && <p className="err-msg">{PWD_ERR_MSG[pwdErr]}</p>}
-                        <div id="signin-support">
-                            <label>
-                                <input type="checkbox" />
-                                Remember me?
-                            </label>
-                            <a href="/dummy-forgot" id="signin-forgot">Forget Password</a>
-                        </div>
-                        <button id="signin-btn" type="submit" onClick={handleClick}>Sign in</button>
-                    </form>
+            {token && (<Navigate to="/" />)}
+            <div id="signin">
+                <div id="signin-left">
+                    <div id="signin-card">
+                        <div id="signin-logo"><b>LOGO</b></div>
+                        <h1>Sign In</h1>
+                        <p id="signin-subtitle">Sign in to stay connected.</p>
+                        <form onSubmit={doLogin}>
+                            {/* Username */}
+                            <div className="input-control">
+                                <label className="input-label" htmlFor="username">Username</label>
+                                <input type="text" className={`${unameErr ? "red-border" : "blue-border"}`} name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                                {unameErr !== 0 && <i className="fa-solid fa-circle-exclamation"></i>}
+                            </div>
+                            {unameErr !== 0 && <p className="err-msg">{UNAME_ERR_MSG[unameErr]}</p>}
+                            {/* Password */}
+                            <div className="input-control">
+                                <label className="input-label" htmlFor="password">Password</label>
+                                <input type="password" className={`${pwdErr ? "red-border" : "blue-border"}`} name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                {pwdErr !== 0 && <i className="fa-solid fa-circle-exclamation"></i>}
+                            </div>
+                            {pwdErr !== 0 && <p className="err-msg">{PWD_ERR_MSG[pwdErr]}</p>}
+                            {/* Support Options */}
+                            <div id="signin-support">
+                                <label>
+                                    <input type="checkbox" />
+                                    Remember me?
+                                </label>
+                                <a href="/dummy-forgot" id="signin-forgot">Forget Password</a>
+                            </div>
+                            {/* Sign-in Button */}
+                            <button id="signin-btn" type="submit" onClick={handleClick}>Sign in</button>
+                        </form>
+                    </div>
+                </div>
+                <div id="signin-right">
+                    <img src={sideImage} alt="side-img" />
                 </div>
             </div>
-            <div id="signin-right">
-                <img src={sideImage} alt="side-img"/>
-            </div>
-        </div>
         </>
     );
 }
